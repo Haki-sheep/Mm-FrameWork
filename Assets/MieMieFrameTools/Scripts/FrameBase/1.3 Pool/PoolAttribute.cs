08f530ea8beadc018f948f255a0406557f0db172
fmz <fmz@example.com>
2026-05-24 14:17:20 +0800

    简化对象池逻辑

--- Assets/MieMieFrameTools/Scripts/FrameBase/1.3 Pool/PoolAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace MieMieFrameWork.Pool
-{
-    using System;
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
-    public class PoolAttribute : Attribute
-    {
-    }
-
-}
--- Assets/MieMieFrameTools/Scripts/FrameBase/1.3 Pool/PoolConfig.cs
+++ Assets/MieMieFrameTools/Scripts/FrameBase/1.3 Pool/PoolConfig.cs
@@ -1,38 +1,41 @@
-namespace MieMieFrameWork
+namespace MieMieFrameWork.Pool
 {
-    using MieMieFrameWork.Pool;
+    using System;
     using UnityEngine;
 
+    #region 对象池特性
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class PoolAttribute : Attribute { }
+
+    #endregion
+
+    #region 对象池配置规则
     /// <summary>
-    /// 对象池相关的扩展方法
+    /// 对象池配置规则
     /// </summary>
+    public static class PoolConfig
+    {
+        public static bool ShouldUsePool<T>() where T : class => ShouldUsePool(typeof(T));
+        public static bool ShouldUsePool(Type type) => type.IsDefined(typeof(PoolAttribute), false);
+    }
+    #endregion
+
+    #region 对象池相关的扩展方法
+
     public static class PoolExtensions
     {
-        /// <summary>
-        /// 将该GameObject放入对象池
-        /// </summary>
-        /// <param name="obj">目标GameObject</param>
         public static void PushGameObjectToPool(this GameObject obj)
         {
             ModuleHub.Instance.GetManager<PoolManager>().PushGameObj(obj);
         }
-        /// <summary>
-        /// 将该Component身上的GameObject放入对象池
-        /// </summary>
-        /// <param name="component">目标Component</param>
         public static void PushGameObjectToPool(this Component component)
         {
             ModuleHub.Instance.GetManager<PoolManager>().PushGameObj(component.gameObject);
         }
-
-        /// <summary>
-        /// 将该Object放入对象池
-        /// </summary>
-        /// <param name="obj">目标对象</param>
         public static void PushObjectToPool(this object obj)
         {
             ModuleHub.Instance.GetManager<PoolManager>().PushObject(obj);
         }
-
     }
+    #endregion
 }
